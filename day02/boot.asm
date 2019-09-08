9abfea0a48450c0af1be705019f9d1305178284b
lsr <1445469285@qq.com>
2019-09-08 21:18:40 +0800

    改变c++项目位置

--- day02/boot.asm
+++ day02/boot.asm
@@ -11,20 +11,21 @@ entry:
 
 readFloppy:
 	; 加载扇区2, 柱面1, 磁头0
-	mov	CH, 1        ; 柱面号1
-	mov	DH, 0        ; 磁头号0
-	mov	CL, 2        ; 扇区号2
+	mov	CH, 1		; 柱面号1
+	mov	DH, 0		; 磁头号0
+	mov	CL, 2		; 扇区号2
 
-	mov	BX, LOAD_ADDR       ; ES:BX 数据存储缓冲区
+	mov	BX, LOAD_ADDR	; ES:BX 数据存储缓冲区
 
-	mov	AH, 0x02      ; AH = 02 表示要做的是读盘操作
-	mov	AL,  1        ; AL 表示要练习读取几个扇区
-	mov	DL, 0         ; 驱动器编号
-	INT	0x13          ; 调用BIOS中断实现磁盘读取功能
+	mov	AH, 0x02	; AH = 02 表示要做的是读盘操作
+	mov	AL, 1		; AL 表示要练习读取几个扇区
+	mov	DL, 0		; 驱动器编号
+	INT	0x13		; 调用BIOS中断实现磁盘读取功能
 
 	JC	fin
 
-	jmp	LOAD_ADDR
+	jmp	LOAD_ADDR	; 当前控制权移交到内存的 LOAD_ADDR 处
+
 fin:
 	HLT
 	jmp  fin
---
+++ day02/kernel.asm
@@ -0,0 +1,28 @@
+; 会被加载到物理内存的8000处
+org	0x8000
+
+entry:
+	mov	ax, 0
+	mov	ss, ax
+	mov	ds, ax
+	mov	es, ax
+	mov	si, msg
+
+
+putloop:
+	mov	al, [si]
+	add	si, 1
+	cmp	al, 0
+	je	fin
+	mov	ah, 0x0e
+	mov	bx, 15
+	int	0x10
+	jmp	putloop
+
+fin:
+	HLT
+	jmp	fin
+
+; 作为内核代码的一部分加载到内存中去的
+msg:
+	DB	"This is Hello World from kernel"

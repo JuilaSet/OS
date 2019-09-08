31306304bc64696e814d82ecc3fcf07f56f2e3b3
lsr <1445469285@qq.com>
2019-09-08 17:34:53 +0800

    finish 1

--- day01/boot.asm
+++ day01/boot.asm
@@ -28,6 +28,22 @@ entry:
     mov  es, ax
     mov  si, msg
 
+
+readFloppy:
+    mov          CH, 1        ;CH 用来存储柱面号
+    mov          DH, 0        ;DH 用来存储磁头号
+    mov          CL, 2        ;CL 用来存储扇区号
+
+    mov          BX, msg       ; ES:BX 数据存储缓冲区
+
+    mov          AH, 0x02      ;  AH = 02 表示要做的是读盘操作
+    mov          AL,  1        ; AL 表示要练习读取几个扇区
+    mov          DL, 0         ;驱动器编号，一般我们只有一个软盘驱动器，所以写死   
+                               ;为0
+    INT          0x13          ;调用BIOS中断实现磁盘读取功能
+
+    jc           error
+
 putloop:
     mov  al, [si]
     add  si, 1
@@ -42,8 +58,11 @@ fin:
     HLT
     jmp  fin
 
+error:
+    mov si, errmsg   ;出现错误打印error
+    jmp   putloop
+
 msg:
-    DB    0x0a,  0x0a
-    db    "hello, world"
-    db    0x0a
-    db    0
+    RESB   64
+errmsg:
+    DB "error"

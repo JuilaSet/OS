cd6725373a4fc09150253f9b8c2154c45d704d97
lsr <35889430+JuilaSet@users.noreply.github.com>
2019-09-17 14:05:49 +0800

    update day5 code

--- day05/foo.asm
+++ day05/foo.asm
@@ -8,31 +8,30 @@ arg2  dd 4
 global _start
 global foo_print
 
-// 程序入口点
+; 程序入口点
 _start: 
-	// 放入第一个和第二个参数, 类型为 00 00 00 00 位, 4字节(c语言中为int)
+	; 放入第一个和第二个参数, 类型为 00 00 00 00 位, 4字节(c语言中为int)
 	mov	eax, dword [arg1]
 	push	eax
 	
-	// 使用堆栈传参
+	; 使用堆栈传参
 	mov	eax, dword [arg2]
 	push	eax
 
-	// 跳转函数, 将返回地址push
+	; 跳转函数, 将返回地址push
 	add	esp, 8
 	mov	ebx, 0
 	mov	eax, 1
 
-	// Unix调用中断
+	; Unix调用中断
 	int	0x80
 
-// c z调用约定
-foo_start: 
-	// 恢复现场
-	mov	edx, [esp + 8]	// 恢复栈顶指针
-	mov	ecx, [esp + 4]	// 指向返回地址, ret指令用于控制EIP指针
+; c z调用约定
+foo_print: 
+	; 恢复现场
+	mov	edx, [esp + 8]	; 恢复栈顶指针
+	mov	ecx, [esp + 4]	; 指向返回地址, ret指令用于控制EIP指针
 	mov	ebx, 1
 	mov	eax, 4
 	int	0x80	
 	ret
-　

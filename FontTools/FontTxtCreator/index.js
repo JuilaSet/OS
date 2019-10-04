97d6017bd647700807fd7a9a16687a863583941b
lsr <1445469285@qq.com>
2019-10-04 14:11:21 +0800

    update tools

--- FontTools/FontTxtCreator/index.js
+++ FontTools/FontTxtCreator/index.js
@@ -29,6 +29,72 @@ let colorCharMap = {
 		ch: "b",
 		color : "#0000FF"
 	},
+	
+	"purpose" : {
+		index : 5,
+		ch: "p",
+		color : "#ff00ff"
+	},
+	
+	"light" : {
+		index : 6,
+		ch: "l",
+		color : "#00ffff"
+	},
+	
+	"white" : {
+		index : 7,
+		ch: "w",
+		color : "#ffffff"
+	},
+	
+	"grey" : {
+		index : 8,
+		ch: "-",
+		color : "#c6c6c6"
+	},
+	
+	"drakred" : {
+		index : 9,
+		ch: "R",
+		color : "#840000"
+	},
+	
+	"drakgreen" : {
+		index : 10,
+		ch: "G",
+		color : "#008400"
+	},
+	
+	"drakyellow" : {
+		index : 11,
+		ch: "Y",
+		color : "#848400"
+	},
+	
+	"drakblue" : {
+		index : 12,
+		ch: "B",
+		color : "#000084"
+	},
+	
+	"drakpurpos" : {
+		index : 13,
+		ch: "P",
+		color : "#840084"
+	},
+	
+	"slate" : {
+		index : 14,
+		ch: "s",
+		color : "#008484"
+	},
+	
+	"drakgrey" : {
+		index : 15,
+		ch: "=",
+		color : "#848484"
+	},
 
 	"default" : {
 		index : -1,
@@ -37,12 +103,17 @@ let colorCharMap = {
 	}
 };
 
+let inActiveColor = "#EEE";
 function getColorIdentifier(name){
 	return (colorCharMap[name] == undefined ? colorCharMap["default"] : colorCharMap[name]);
 }
 
 // 数据层
 let widthN = 8, heightN = 16;
+let canvas;
+let w = 37.75 // canvas.width / widthN;
+let h = 37.75 // (canvas.height - 50) / heightN;
+
 function createGrid(){
 	let fontGrid = new Array();
 	for(let i =0; i < heightN; ++i){
@@ -66,17 +137,85 @@ function initGrid(fontGrid){
 					this.state = 1;
 				},
 				inActive : function(){
-					this.rect.fill = "#FFF";
+					this.rect.fill = inActiveColor;
 					this.state = 0;
 				},
 				applyBlock : function(){
-					this.rect.fill = (this.state == 1 ? "#222" : "#FFF");
+					this.rect.fill = (this.state == 1 ? getColorIdentifier(this.color).color : inActiveColor);
+				},
+				assgin : function(grid){
+					this.state = grid.state;
+					this.color = grid.color;
 				}
 			};
 }
 
+function moveLeft(fontGrid){
+	for(let i = 0; i < heightN; ++i)
+		for(let j = 0; j < widthN; ++j){
+		if(j == widthN - 1)
+			fontGrid[i][j].assgin({ state: 0, color: "default" });
+		else
+			fontGrid[i][j].assgin(fontGrid[i][j + 1]);
+	}
+}
+
+function moveRight(fontGrid){
+	for(let i = 0; i < heightN; ++i)
+		for(let j = widthN - 1; j >= 0; --j){
+		if(j == 0)
+			fontGrid[i][j].assgin({ state: 0, color: "default" });
+		else
+			fontGrid[i][j].assgin(fontGrid[i][j - 1]);
+	}
+}
+
+function moveUp(fontGrid){
+	for(let i = 0; i < heightN; ++i)
+		for(let j = 0; j < widthN; ++j){
+		if(i == heightN - 1)
+			fontGrid[i][j].assgin({ state: 0, color: "default" });
+		else
+			fontGrid[i][j].assgin(fontGrid[i + 1][j]);
+	}
+}
+
+function moveDown(fontGrid){
+	for(let i = heightN - 1; i >= 0; --i)
+		for(let j = 0; j < widthN; ++j){
+		if(i == 0)
+			fontGrid[i][j].assgin({ state: 0, color: "default" });
+		else
+			fontGrid[i][j].assgin(fontGrid[i - 1][j]);
+	}
+}
+
+function generatrText(){
+	let string = "";
+	for(var i=0; i<heightN; ++i){
+		for(var j=0; j<widthN; ++j){
+			if(grid[i][j].state == 1){
+				string += getColorIdentifier(grid[i][j].color).ch;
+			}else{
+				string += '.';
+			}
+		}
+		string += '\n';
+	}
+	output.innerHTML = string;
+}
+
+// 根据数据应用到界面
+function applyGrid(fontGrid){
+	for(let i = 0; i < heightN; ++i)
+		for(let j = 0; j < widthN; ++j){
+			grid[i][j].applyBlock();
+		}
+	generatrText();
+	canvas.redraw();
+}
+
 let grid = createGrid();
-let canvas;
 
 // 显示层
 oCanvas.domReady(function () {
@@ -84,15 +223,23 @@ oCanvas.domReady(function () {
 	let output = document.getElementById("output");
 	let canvasSwitcher = document.getElementById("canvasSwitcher");
 	let canvasCleaner = document.getElementById("canvasCleaner");
+	
+	let copyer = document.getElementById("copy");
 	let colorSelectBoard = document.getElementById("colorSelectBoard");
+
+	let cleft = document.getElementById("cleft");
+	let cup = document.getElementById("cup");
+	let cright = document.getElementById("cright");
+	let cdown = document.getElementById("cdown");
 
 	canvas = oCanvas.create({
 		canvas: "#canvas",
-		background: "#FFF"
+		background: inActiveColor
 	});
-
-	let w = canvas.width / widthN;
-	let h = (canvas.height - 50) / heightN;
+	
+	// 画布大小
+	canvas.width = w * widthN + 2;
+	canvas.height = h * heightN + 2;
 
 	function floorPosition(x, y){
 		let pos = {x:0, y:0};
@@ -117,7 +264,7 @@ oCanvas.domReady(function () {
 				y: 1 + i * h,
 				width: w,
 				height: h,
-				fill: "#FFF",
+				fill: inActiveColor,
 				stroke: "outside 1px rgba(0.0, 0.0, 1, 0.5)"
 			});
 
@@ -135,6 +282,30 @@ oCanvas.domReady(function () {
 
 	// 控制器
 
+	// 向左移动
+	cleft.onclick = function(){
+		moveLeft(grid);
+		applyGrid(grid);
+	}
+	cright.onclick = function(){
+		moveRight(grid);
+		applyGrid(grid);
+	}
+	cup.onclick = function(){
+		moveUp(grid);
+		applyGrid(grid);
+	}
+	cdown.onclick = function(){
+		moveDown(grid);
+		applyGrid(grid);
+	}
+
+	// 复制内容
+	copyer.onclick = function(){
+		output.select();
+		document.execCommand("copy");
+	}
+
 	// 清除内容
 	canvasCleaner.onclick = function(){
 		for(let i = 0; i < heightN; ++i)
@@ -145,6 +316,7 @@ oCanvas.domReady(function () {
 		generatrText();
 	}
 
+	// 切换颜色
 	let colorSwitch = "default";
 	var cleanMode = false;
 	colorSelectBoard.addEventListener('change',function(){
@@ -156,21 +328,6 @@ oCanvas.domReady(function () {
 		colorSwitch = this.value;
 	}, false);
 
-	function generatrText(){
-		let string = "";
-		for(var i=0; i<heightN; ++i){
-			for(var j=0; j<widthN; ++j){
-				if(grid[i][j].state == 1){
-					string += getColorIdentifier(grid[i][j].color).ch;
-				}else{
-					string += '.';
-				}
-			}
-			string += '\n';
-		}
-		output.innerHTML = "结果: \n" + string;
-	}
-
 	/*let textContainer = canvas.display.text({
 		x: 157,
 		y: 196,
@@ -178,7 +335,7 @@ oCanvas.domReady(function () {
 		align: "center",
 		font: "bold 25px/1.5 sans-serif",
 		text: "(0, 0)",
-		fill: "#FFF"
+		fill: inActiveColor
 	});*/
 
 	// canvas.addChild(textContainer);
@@ -200,6 +357,7 @@ oCanvas.domReady(function () {
 						grid[pos.y][pos.x].inActive();
 					}
 					generatrText();
+					// applyGrid(grid);
 				}
 			}
 		}
